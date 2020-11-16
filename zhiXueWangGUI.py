0167f89c9bf5bcdc80a75d8f1c47c10266460f46
chafiprc <1538153895@qq.com>
2020-11-16 12:19:52 +0800

    Add new features

--- zhiXueWangGUI.py
+++ zhiXueWangGUI.py
@@ -9,11 +9,11 @@ class zxwWindow:
     
     def __init__(self,init_window_name):
         self.init_window_name = init_window_name
-        self.username = str()
-        self.passwd = str()
-        self.person = zxw.student.student.Student
-        self.nowExam = zxw.models.Exam
-        self.allExams = zxw.models.ExtendedList
+        self.username : str() = None
+        self.passwd : str() = None
+        self.person : zxw.student.student.Student = None
+        self.nowExam : zxw.models.Exam = None
+        self.allExams : zxw.models.ExtendedList = None
         self.fonts = tkfont.Font(family='Arial')
     
     def init_window(self):
@@ -36,10 +36,10 @@ class zxwWindow:
 
         #标签
         self.usernameLabel = tk.Label(self.init_window_name, text='账号:', font=self.fonts)
-        self.usernameLabel.grid(row=1,column=1)
+        self.usernameLabel.grid(row=1,column=0)
 
         self.pswdLabel = tk.Label(self.init_window_name, text='密码:', font=self.fonts)
-        self.pswdLabel.grid(row=6,column=1)
+        self.pswdLabel.grid(row=6,column=0)
 
         self.selectHelp = tk.Label(self.init_window_name, text='^试卷选择^\n选定后按切换试卷按钮', font=self.fonts)
         self.selectHelp.grid(row=13, column=9)
@@ -70,8 +70,10 @@ class zxwWindow:
         self.exitBut.grid(row=13, column=1)
 
         self.exitBut = tk.Button(self.init_window_name, text='清空文本框', bg="lightblue", width=8, font=self.fonts, command=self.clear_Text)
-        self.exitBut.grid(row=14, column=2)
+        self.exitBut.grid(row=14, column=1)
 
+        self.getPaperBut = tk.Button(self.init_window_name, text='获取原卷', bg='lightblue', width=8, font=self.fonts, command=self.callSubSubjectWindow)
+        self.getPaperBut.grid(row=14, column=3)
 
         #列表
         self.examList = tk.Listbox(self.init_window_name, selectmode = tk.BROWSE, width=40, height=8, font=self.fonts)
@@ -146,16 +148,62 @@ class zxwWindow:
 
     def about(self):
         self.write_log_to_Text('\nzhiXueWangGUI:\nMade by chafiprc.')
+        self.write_log_to_Text('(仅供连江一中内部使用)',False)
         self.write_log_to_Text('\n免责声明:\n本软件仅供个人学习、研究。我们不保证内容的正确性。通过使用本软件随之而来的风险与软件制作者无关\n软件仅用于个人学习、研究以及其他非商业性或非盈利性用途，但同时应遵守著作权法及其他相关法律的规定，不得相关人的合法权利。\n如果用户下载、使用本软件，即表明用户信任该软件作者\n对任何原因在使用本软件时可能对用户自己或他人造成的任何形式的损失和伤害,软件制作者不承担任何责任。')
+
+
+    def callSubSubjectWindow(self):
+        if self.nowExam == None:
+            self.write_log_to_Text('您还未登录,请登录之后再试')
+        else:
+            self.subWindow = tk.Tk()
+            self.subWindow.title('选择科目')
+            self.subjectList = ['语文','数学','英语','物理','化学','地理','历史','政治','生物']
+
+            #标签
+            helpLabel = tk.Label(self.subWindow, text='请注意:部分久远考试无法获取原卷', font=self.fonts)
+            helpLabel.grid(row=0,column=0)
+
+            #按钮
+            chooseSubjectBut = tk.Button(self.subWindow, text='选定科目', width=8, font=self.fonts, command=self.__subSubjectChoose)
+            chooseSubjectBut.grid(row=11,column=0)
+
+            #列表
+            self.subjectListbox = tk.Listbox(self.subWindow, selectmode = tk.BROWSE, width=20, height=9, font=self.fonts)
+            for i in self.subjectList:
+                self.subjectListbox.insert(tk.END,i)
+            self.subjectListbox.grid(row=1, column=0, rowspan=9)
+            self.subWindow.mainloop()
+
+    def __subSubjectChoose(self):
+        selectSub = self.subjectList[self.subjectListbox.curselection()[0]]
+        try:
+            paperUrl = self.person.get_original(selectSub,self.nowExam.id)
+        except KeyError:
+            self.write_log_to_Text('无法获取原卷列表,请稍后再试')
+        else:
+            if paperUrl == []:
+                self.write_log_to_Text('抱歉，无法获取' + selectSub + '原卷地址')
+            else:
+                self.write_log_to_Text('\n' + selectSub +'-试卷链接为:')
+                for i in paperUrl:
+                    self.write_log_to_Text(i,False)
+        finally:
+            self.subWindow.destroy()
+        
+    
 
     def get_current_time(self):
         current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         return current_time
 
-    def write_log_to_Text(self,logmsg):
+    def write_log_to_Text(self,logmsg,timeVis=True):
         global LOG_LINE_NUM
         current_time = self.get_current_time()
-        logmsg_in = str(current_time) +" " + str(logmsg) + "\n"      #换行
+        if timeVis:
+            logmsg_in = str(current_time) +" " + str(logmsg) + "\n"      #换行
+        else:
+            logmsg_in = str(logmsg) + "\n"
         if LOG_LINE_NUM <= 8:
             self.log_data.insert(tk.END, logmsg_in)
             LOG_LINE_NUM = LOG_LINE_NUM + 1
